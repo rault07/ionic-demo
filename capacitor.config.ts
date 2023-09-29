f9e7a364c240c4e6923887a4c92fda0f1fe678a4
Raul Tristan <raul.tristan.07@gmail.com>
2023-09-28 23:09:30 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app-demo-tristan',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
